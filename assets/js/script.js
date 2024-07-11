e7de5842f31e4e839b6e35ff59b7cd5e2858e7fe
Nicolas-Tolosa <nicolas.tolosar@gmail.com>
2024-07-10 23:41:47 -0400

    actualizacion del javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ const totalTareasRealizadas = document.querySelector("#total-tareas-realizadas")
 const tablaTareas = document.querySelector("#tabla-body");
 
 let listaTareas = [
-    { id: Date.now(), tarea: "lorem ipsum dolor" },
-    { id: Date.now() + 1, tarea: "lorem ipsum dolor" },
-    { id: Date.now() + 2, tarea: "lorem ipsum dolor" }];
-let tareasCompletadas = [];
+    { id: Date.now(), tarea: "lorem ipsum dolor", completada: false },
+    { id: Date.now() + 1, tarea: "lorem ipsum dolor", completada: false },
+    { id: Date.now() + 2, tarea: "lorem ipsum dolor", completada: false }
+];
 
 function renderTareas() {
     let html = "";
@@ -17,26 +17,25 @@ function renderTareas() {
         `<tr>
             <td><p>${tarea.id}</p></td>
             <td><p>${tarea.tarea}</p></td>
-            <td><input onclick="completarTarea(${tarea.id})" type="checkbox"></td>
+            <td><input onclick="completarTarea(${tarea.id})" type="checkbox" ${tarea.completada ? 'checked' : ''}></td>
             <td><button onclick="borrarTarea(${tarea.id})">Eliminar Tarea</button></td>
         </tr>`;
     }
     tablaTareas.innerHTML = html;
     totalTareas.innerHTML = `${listaTareas.length}`;
-    totalTareasRealizadas.innerHTML = `${tareasCompletadas.length}`;
+    totalTareasRealizadas.innerHTML = `${listaTareas.filter(tarea => tarea.completada).length}`;
 }
 
 botonAgregarTarea.addEventListener("click", () => {
-    if (inputTarea.value === ""){
+    if (inputTarea.value === "") {
         alert("Por favor escribe una tarea")
     } else {
-        let nuevaTarea = { id: Date.now(), tarea: inputTarea.value };
+        let nuevaTarea = { id: Date.now(), tarea: inputTarea.value, completada: false };
         listaTareas.push(nuevaTarea);
         inputTarea.value = "";
         console.log(listaTareas);
         renderTareas();
     }
-
 });
 
 function borrarTarea(idTarea) {
@@ -47,9 +46,8 @@ function borrarTarea(idTarea) {
 
 function completarTarea(idTarea) {
     const index = listaTareas.findIndex(tarea => tarea.id === idTarea);
-    if (index !== -1) { // Comprueba que se haya encontrado una tarea
-        const [tarea] = listaTareas.splice(index, 1); //Crea la variable tarea haciendo uso de la desestructuracion del array listaTareas
-        tareasCompletadas.push(tarea);
+    if (index !== -1) {
+        listaTareas[index].completada = !listaTareas[index].completada;
         renderTareas();
     }
 }
